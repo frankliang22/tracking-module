47ae276e9e6c24d2a7abbc026849793c531a2222
frank liang <348017715@qq.com>
2015-02-04 15:32:45 +0800

    add a related field

--- tracking.py
+++ tracking.py
@@ -7,26 +7,65 @@ class tracking(osv.Model):
     
 	_name = 'tracking.tracking'
 
+	_sql_constraints = [('name_unique','unique(name)','You can not create a duplicate serial number')]
+
+
 	_columns = {                                                                                                                                                                                                                                                                                             
-		'tier' : fields.selection([('Production Reference','Production Reference'),('Golden Reference','Golden Reference'),('Engineeering Sample','Engineering Sample'),('Mechanical Sample','Mechanical Sample'),('Failure Model Sample','Failure Model Sample'),('SW Test Sample','SW Test Sample')],'Tier'),
-		'project_id' : fields.many2one('project.project','Project'),
-		'version': fields.many2one('res.states', string="Version"),
-		'condition' : fields.selection([('Active','Active'),('Defective-Unrepairable','Defective-Unrepairable'),('Defective-Needs Repair','Defective-Needs Repair'),('Destroy/Discard/EOL','Destroy/Discard/EOL')],'Condition'),
+		'tier' : fields.selection([
+			('Production Reference','Production Reference'),
+			('Golden Reference','Golden Reference'),
+			('Engineeering Sample','Engineering Sample'),
+			('Mechanical Sample','Mechanical Sample'),
+			('Failure Model Sample','Failure Model Sample'),
+			('SW Test Sample','SW Test Sample')],'Tier'),
+		'project_id' : fields.char('Project/Part Number'),
+		'version': fields.char('Version/NPI Stage'),
+		'condition' : fields.selection([
+			('Active','Active'),
+			('Defective-Unrepairable','Defective-Unrepairable'),
+			('Defective-Needs Repair','Defective-Needs Repair'),
+			('Destroy/Discard/EOL','Destroy/Discard/EOL')],'Condition'),
 		'sample_data1' : fields.binary('Sample Data 1'),
 		'sample_data2' : fields.binary('Sample Data 2'),
 		'sample_data3' : fields.binary('Sample Data 3'),
-		'type' : fields.selection([('Electronics','Electronics'),('Driver','Driver'),('System','System')],'Type',required=True),
-		'electronics_type' : fields.selection([('Audio','Audio'),('Functional','Functional'),('Wireless','Wireless'),('All','All')],'Sub-Type'),
-		'driver_type' : fields.selection([('Frequency Response','Frequency Response'),('Impedance','Impedance'),('Distortion','Distortion'),('Rub & Buzz','Rub & Buzz'),('All','All')],'Sub-Type'),
-		'system_type' : fields.selection([('Frequency Response','Frequency Response'),('Impedance','Impedance'),('Distortion','Distortion'),('Rub & Buzz','Rub & Buzz'),('Airleak','Airleak'),('Cosmetic','Cosmetic'),('Wireless','Wireless'),('Functional','Functional'),('All','All')],'Sub-Type'),
-        'verification_interval' : fields.char(string="Verification Interval",size=256),
-        'name' : fields.char('Serial Number',required=True),
+		'type' : fields.selection([
+			('Electronics','Electronics'),
+			('Driver','Driver'),
+			('System','System')],'Type',required=True),
+		'electronics_type' : fields.selection([
+			('Audio','Audio'),
+			('Functional','Functional'),
+			('Wireless','Wireless'),
+			('All','All')],'Sub-Type'),
+		'driver_type' : fields.selection([
+			('Frequency Response','Frequency Response'),
+			('Impedance','Impedance'),
+			('Distortion','Distortion'),
+			('Rub & Buzz','Rub & Buzz'),
+			('All','All')],'Sub-Type'),
+		'system_type' : fields.selection([
+			('Frequency Response','Frequency Response'),
+			('Impedance','Impedance'),
+			('Distortion','Distortion'),
+			('Rub & Buzz','Rub & Buzz'),
+			('Airleak','Airleak'),
+			('Cosmetic','Cosmetic'),
+			('Wireless','Wireless'),
+			('Functional','Functional'),
+			('All','All')],'Sub-Type'),
+        'verification_interval' : fields.integer(string="Verification Interval"),
+        'name' : fields.char('Serial Number',required=True,store=True),
         'production_serial_number' : fields.char(string="Production Serial Number",size=256),
         'sample_ids' : fields.one2many('tracking.sample','tracking_id','Sample Data'),
         'date_ids' : fields.one2many('tracking.verification','tracking_id','Verification History For * Tier'),
-        'check_in_ids' : fields.one2many('tracking.check_in','serial_number',string='Check In'),
-        'check_out_ids' : fields.one2many('tracking.check_out','serial_number',string='Check Out'),
+        'check_ids' : fields.one2many('tracking.check','name',string='Check In'),
+        'location' : fields.char(string="Location",size=256),
+        'partner_id': fields.char('Customer'),
+        'release_date' : fields.date('Release Date'),
+        'user_id' : fields.char('Approver'),
+        'unit' : fields.selection([('days','days'),('weeks','weeks'),('months','months'),('years','years'),('pieces','pieces')],'Interval Unit'),
     }
+
 
 class sample(osv.Model):
 	
@@ -44,7 +83,7 @@ class verification(osv.Model):
 
 	_columns = {
 	    'tracking_id' : fields.many2one('tracking.tracking',ondelete='cascade',string="Tracking"),
-	    'partner_id' : fields.many2one('res.partner','Who'),
+	    'user_id' : fields.many2one('res.users','Who'),
 	    'date' : fields.date('Date'),
 	    'verification_ids' : fields.many2many('tracking.tracking','tracking_verification_rel','verification_id','tracking_id',string='Related Sample'),
 	}
@@ -61,64 +100,26 @@ class location(osv.Model):
 	    'with_department_member' : fields.many2one('res.partner','With Department Member'),
 	}
 
-class check_in(osv.Model):
 
-	_name = 'tracking.check_in'
 
-	_columns = {
-	    'serial_number': fields.many2one('tracking.tracking',  string="Serial Number",required=True),   
-        'time_now' : fields.datetime('Time',readonly=True),
-        'create_uid': fields.many2one('res.users', 'Created by', readonly=True),
-	}
-    
-	_defaults = {
-		'time_now': lambda self, cr, uid, context={}: context.get('time_now', datetime.now().strftime("%Y-%m-%d %H:%M:%S")),
-		}
+class _check_in_out(osv.Model):
 
-class check_out(osv.Model):
+	_name = 'tracking.check'
 
-	_name = 'tracking.check_out'
+	_sql_constraints = [('name_unique','unique(name)','You can not create a duplicate serial number')]
 
 	_columns = {
-	    'tracking_id' : fields.many2one('tracking.tracking','Tracking'),
-	    'serial_number': fields.many2one('tracking.tracking',  string="Serial Number",required=True),
-	    'create_uid': fields.many2one('res.users', 'Created by', readonly=True),
-	    'time_now' : fields.datetime('Time',readonly=True),
-	    'use' : fields.selection([('On Production Line','On Production Line'),('With Department Member','With Department Member'),('Lent Out','Lent Out')],'Use'),
-	    'who' : fields.char('Who'),
-	    #'state': field.selection([('Checked In','Checked in'),('Checked out','Checked out')],'State'),
+		'name': fields.many2one('tracking.tracking',string="Serial Number",required=True, store=True),
+		'state' : fields.selection([('Checked-in','Checked-in'),('Checked-out','Checked-out')],"State"),
+		'create_uid': fields.many2one('res.users', 'Created by', readonly=True),
+		'time_now' : fields.datetime('Time',readonly=True),
+		'location' : fields.char('Location'),
+		'who' : fields.char('Who'),
+		'project_id' : fields.related('name','project_id', readonly=True, type='char', string="Project/Part Number"),
+			
 	}
 
 	_defaults = {
 		'time_now' : lambda self, cr, uid, context={}: context.get('time_now', datetime.now().strftime("%Y-%m-%d %H:%M:%S")),
-		}
-
-class check(osv.Model):
-
-	_name = 'tracking.check'
-
-	def _check_in_out(self, cr, uid, ids, field_name, arg, context=None):
-		res = {}
-		for i in self.browse(cr, uid, ids,context=context):
-			if i.serial_number.id and i.state == "checked-in":
-				res[i.id]= str(i.state)
-			else:
-				res[i.id]= "check-in" 
-		return res
-
-	def _check_io_(self, cr, uid, ids, field_name, arg, context=None):
-		checkin = "checked_in"
-		checkout = "checked_out" 
-
-		if self.browse(cr, uid, ids,context=context).serial_number and self.browse(cr, uid, ids,context=context).state == "checked-in":
-			return  str(checkout)
-		else: #serial_number.id and state == "checked_out":
-			return str(checkin)
-	_columns = {
-		'serial_number': fields.many2one('tracking.tracking',string="Serial Number",required=True),
-		'state' : fields.function(_check_io_,type='char',string="State"),
-	}
-
-	_defaults = {
-		'state' : "checked-in"
+		'state' : 'Checked-in',
 	}
